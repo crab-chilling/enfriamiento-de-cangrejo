a1c37a18a79ea75414d5d98ac1200a92f215d01e
yoann-cathelain <95906348+yoann-cathelain@users.noreply.github.com>
2024-05-26 19:40:06 +0200

    Feat/jwt (#8)
    * feat/jwt : JWT return from login GET request
    
    * feat/jwt: UserCache for authenticated users
    
    * feat/jwt: add jwt read on store endpoints

--- Atelier2/CardForge/src/main/java/cpe/asi/cardforge/controller/StoreItemController.java
+++ Atelier2/CardForge/src/main/java/cpe/asi/cardforge/controller/StoreItemController.java
@@ -2,19 +2,13 @@ package cpe.asi.cardforge.controller;
 
 import cpe.asi.cardforge.dto.CardDTO;
 import cpe.asi.cardforge.dto.StoreItemDTO;
-import cpe.asi.cardforge.entity.Card;
-import cpe.asi.cardforge.entity.Kuser;
-import cpe.asi.cardforge.entity.StoreItem;
-import cpe.asi.cardforge.error.NotFoundException;
-import cpe.asi.cardforge.repository.CardRepository;
 import cpe.asi.cardforge.repository.StoreItemRepository;
-import cpe.asi.cardforge.repository.UserRepository;
 import cpe.asi.cardforge.service.StoreItemService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -23,14 +17,10 @@ public class StoreItemController {
 
     private final StoreItemService storeItemService;
     private final StoreItemRepository storeItemRepository;
-    private final UserRepository userRepository;
-    private final CardRepository cardRepository;
 
-    public StoreItemController(StoreItemService storeItemService, StoreItemRepository storeItemRepository,UserRepository userRepository, CardRepository cardRepository) {
+    public StoreItemController(StoreItemService storeItemService, StoreItemRepository storeItemRepository) {
         this.storeItemService = storeItemService;
         this.storeItemRepository = storeItemRepository;
-        this.userRepository = userRepository;
-        this.cardRepository = cardRepository;
     }
 
     @GetMapping("/all")
@@ -44,66 +34,15 @@ public class StoreItemController {
     }
 
     @PostMapping("/purchase/{id}")
-    public StoreItemDTO purchase(@PathVariable Long id) {
+    public StoreItemDTO purchase(@RequestHeader(name="Authorization") String token, @PathVariable Long id) throws IOException {
         log.info("Purchasing store item");
-        Optional<StoreItem> optItem = storeItemRepository.findById(id);
-        if (optItem.isEmpty()) {
-            throw new NotFoundException("Store item not found");
-        }
-
-        Long idBuyer = 1L;
-        Optional<Kuser> optBuyer = userRepository.findById(idBuyer);
-
-        if (optBuyer.isEmpty()) {
-            throw new NotFoundException("User not found");
-        }
-
-        float price = optItem.get().getPrice();
-        float wallet = optBuyer.get().getWallet();
-
-        if (wallet < price) {
-            throw new NotFoundException("Not enough money");
-        }
-
-        optBuyer.get().setWallet(wallet - price);
-        userRepository.save(optBuyer.get());
-        Kuser seller = optItem.get().getUser();
-        seller.setWallet(seller.getWallet() + price);
-        userRepository.save(seller);
-        storeItemRepository.delete(optItem.get());
-
-        return storeItemService.convertToDTO(optItem.get());
+        return storeItemService.convertToDTO(storeItemService.purchase(token, id));
     }
 
     @PostMapping("/sell/{price}")
-    public StoreItemDTO sell(@RequestBody CardDTO cardDTO, @PathVariable float price) {
+    public StoreItemDTO sell(@RequestHeader(name="Authorization") String token, @RequestBody CardDTO cardDTO, @PathVariable float price) throws IOException {
         log.info("Selling store item");
-        Optional<Card> optCard = cardRepository.findById(cardDTO.getId());
-
-        if (optCard.isEmpty()) {
-            throw new NotFoundException("Card not found");
-        }
-
-        Long idSeller = 1L;
-        Optional<Kuser> optSeller = userRepository.findById(idSeller);
-
-        if (optSeller.isEmpty()) {
-            throw new NotFoundException("User not found");
-        }
-
-        boolean possesCard = optSeller.get().getCards().contains(optCard.get());
-        if (!possesCard) {
-            throw new NotFoundException("User does not own card");
-        }
-
-        StoreItem storeItem = new StoreItem();
-        storeItem.setCard(optCard.get());
-        storeItem.setPrice(price);
-        storeItem.setUser(optSeller.get());
-
-        storeItemRepository.save(storeItem);
-
-        return storeItemService.convertToDTO(storeItem);
+        return storeItemService.convertToDTO(storeItemService.sell(token, price, cardDTO.getId()));
     }
 
 
--- Atelier2/CardForge/src/main/java/cpe/asi/cardforge/service/StoreItemService.java
+++ Atelier2/CardForge/src/main/java/cpe/asi/cardforge/service/StoreItemService.java
@@ -1,18 +1,36 @@
 package cpe.asi.cardforge.service;
 
 import cpe.asi.cardforge.dto.StoreItemDTO;
+import cpe.asi.cardforge.entity.Card;
+import cpe.asi.cardforge.entity.Kuser;
 import cpe.asi.cardforge.entity.StoreItem;
+import cpe.asi.cardforge.error.NotFoundException;
+import cpe.asi.cardforge.repository.CardRepository;
+import cpe.asi.cardforge.repository.StoreItemRepository;
+import cpe.asi.cardforge.repository.UserRepository;
+import cpe.asi.cardforge.security.JwtUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.Optional;
 
 @Slf4j
 @Service
 public class StoreItemService {
+
+    private final StoreItemRepository storeItemRepository;
+    private final UserRepository userRepository;
+    private final CardRepository cardRepository;
     private final ModelMapper modelMapper;
 
-    public StoreItemService() {
+    public StoreItemService(UserRepository userRepository, StoreItemRepository storeItemRepository, CardRepository cardRepository) {
         this.modelMapper = new ModelMapper();
+        this.storeItemRepository = storeItemRepository;
+        this.userRepository = userRepository;
+        this.cardRepository = cardRepository;
     }
 
     public StoreItemDTO convertToDTO(StoreItem storeItem) {
@@ -22,4 +40,66 @@ public class StoreItemService {
     public StoreItem convertToEntity(StoreItemDTO storeItemDTO) {
         return modelMapper.map(storeItemDTO, StoreItem.class);
     }
+
+    public StoreItem purchase(String token, Long id) throws IOException {
+        Optional<StoreItem> optItem = storeItemRepository.findById(id);
+        if (optItem.isEmpty()) {
+            throw new NotFoundException("Store item not found");
+        }
+
+        String[] chunks = token.split("\\.");
+        String base64EncodedBody = chunks[1];
+        Long idBuyer = JwtUtils.getJwtIdFromBody(base64EncodedBody, new Base64(true));
+        Optional<Kuser> optBuyer = userRepository.findById(idBuyer);
+
+        if (optBuyer.isEmpty()) {
+            throw new NotFoundException("User not found");
+        }
+
+        float price = optItem.get().getPrice();
+        float wallet = optBuyer.get().getWallet();
+
+        if (wallet < price) {
+            throw new NotFoundException("Not enough money");
+        }
+
+        optBuyer.get().setWallet(wallet - price);
+        userRepository.save(optBuyer.get());
+        Kuser seller = optItem.get().getUser();
+        seller.setWallet(seller.getWallet() + price);
+        userRepository.save(seller);
+        storeItemRepository.delete(optItem.get());
+        return optItem.get();
+    }
+
+    public StoreItem sell(String token, float price, Long id) throws IOException {
+        Optional<Card> optCard = cardRepository.findById(id);
+
+        if (optCard.isEmpty()) {
+            throw new NotFoundException("Card not found");
+        }
+
+        String[] chunks = token.split("\\.");
+        String base64EncodedBody = chunks[1];
+        Long idSeller = JwtUtils.getJwtIdFromBody(base64EncodedBody, new Base64(true));
+        Optional<Kuser> optSeller = userRepository.findById(idSeller);
+
+        if (optSeller.isEmpty()) {
+            throw new NotFoundException("User not found");
+        }
+
+        boolean possesCard = optSeller.get().getCards().contains(optCard.get());
+        if (!possesCard) {
+            throw new NotFoundException("User does not own card");
+        }
+
+        StoreItem storeItem = new StoreItem();
+        storeItem.setCard(optCard.get());
+        storeItem.setPrice(price);
+        storeItem.setUser(optSeller.get());
+
+        return storeItemRepository.save(storeItem);
+    }
+
+
 }
