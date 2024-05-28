54d8cd64258d8be302683525692a18fdda2272ba
yoann <yoanncathelain23@gmail.com>
2024-05-28 23:21:53 +0200

    feat/microservices: project cleanup

--- Atelier3/atelier3/Projet/core/src/main/java/chilling/exception/TooPoorException.java
+++
@@ -1,7 +0,0 @@
-package fr.crab.chilling.exception;
-
-public class TooPoorException extends RuntimeException {
-    public TooPoorException(String message) {
-        super("Too Poor : " + message);
-    }
-}
