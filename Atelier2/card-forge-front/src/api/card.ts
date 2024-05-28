f446dbaa1150d2c0bdbfa96518f7867344a0a8c2
LilianAndres <lilian.andres02@gmail.com>
2024-05-28 09:34:02 +0200

    feat: add on sale user cards info

--- Atelier2/card-forge-front/src/api/card.ts
+++ Atelier2/card-forge-front/src/api/card.ts
@@ -1,13 +1,12 @@
 import { AxiosResponse } from "axios";
 import axiosInstance from "./conf";
-import { IUser } from "@/types/User";
 import { ICard } from "@/types/Card";
 
 export async function getUserCardCollection(): Promise<ICard[]> {
   try {
-    const response: AxiosResponse<IUser> = await axiosInstance.get("/user/");
-    const data: IUser = response.data;
-    return data.cards || [];
+    const response: AxiosResponse<ICard[]> =
+      await axiosInstance.get("/user/cards");
+    return response.data;
   } catch (error) {
     console.error("Error fetching user card collection:", error);
     return [];
