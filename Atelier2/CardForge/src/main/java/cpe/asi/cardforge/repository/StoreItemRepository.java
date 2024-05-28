1c5cae535aff443aa59571aeeb0fc4cbee384047
LilianAndres <lilian.andres02@gmail.com>
2024-05-28 09:03:30 +0200

    feat: get on sale user cards

--- Atelier2/CardForge/src/main/java/cpe/asi/cardforge/repository/StoreItemRepository.java
+++ Atelier2/CardForge/src/main/java/cpe/asi/cardforge/repository/StoreItemRepository.java
@@ -1,9 +1,14 @@
 package cpe.asi.cardforge.repository;
 
+import cpe.asi.cardforge.entity.Kuser;
 import cpe.asi.cardforge.entity.StoreItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StoreItemRepository extends JpaRepository<StoreItem, Long> {
+
+    List<StoreItem> findAllByUser(Kuser user);
 }
