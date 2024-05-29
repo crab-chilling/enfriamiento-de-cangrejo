4a41ff36f2168d3b63f71c0f5701b85db878843e
yoann <yoanncathelain23@gmail.com>
2024-05-29 09:41:28 +0200

    feat/microservices: microservices done

--- Atelier3/crab-services/user-service/src/main/java/fr/crab/user/configuration/UserConfiguration.java
+++ Atelier3/crab-services/user-service/src/main/java/fr/crab/user/configuration/UserConfiguration.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EntityScan(basePackages = {"fr.crab.entity"})
+@EnableJpaRepositories(basePackages = {"fr.crab.repository", "fr.crab.user.repository"})
 @ComponentScan(basePackages = {"fr.crab"})
 @Import(value = {WebSecurityConfig.class})
 public class UserConfiguration {
