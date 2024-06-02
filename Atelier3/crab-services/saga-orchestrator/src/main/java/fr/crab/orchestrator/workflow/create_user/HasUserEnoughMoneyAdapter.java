ff091f42946a54e42a75f30d2fc3953584594f77
primplouf <79855050+primplouf@users.noreply.github.com>
2024-06-02 23:06:13 +0200

    Saga orchestrator (#19)
    * [SAD] Adding saga orchestrator flows
    
    * [SAD] Saga orchestrator subscribes to Eureka Server
    
    * [SAD] Adding create user workflow steps
    
    * [SAD] Adding C4Model diagram]

---
+++ Atelier3/crab-services/saga-orchestrator/src/main/java/fr/crab/orchestrator/workflow/create_user/HasUserEnoughMoneyAdapter.java
@@ -0,0 +1,27 @@
+package fr.crab.orchestrator.workflow.create_user;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class HasUserEnoughMoneyAdapter implements JavaDelegate {
+    private final WebClient webClient;
+
+    @Value("${gateway.url}")
+    private String gatewayUrl;
+
+    private ModelMapper modelMapper;
+
+    public HasUserEnoughMoneyAdapter() {
+        this.modelMapper = new ModelMapper();
+        this.webClient = WebClient.create();
+    }
+
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        //
+    }
+}
